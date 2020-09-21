cfd689c76bfe60e3bf414da181ccb19c4c7e635c
deepcloudlabs <deepcloudlabs@gmail.com>
2020-09-21 07:45:55 -0700

    rename new.features to module01-new.features

---
+++ module02-thread.programming.in.c++/cmake-build-debug/CMakeFiles/module02_thread_programming_in_c__.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/module02_thread_programming_in_c__.dir/main.cpp.o"
+  "module02_thread_programming_in_c__"
+  "module02_thread_programming_in_c__.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/module02_thread_programming_in_c__.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ module02-thread.programming.in.c++/cmake-build-debug/CMakeFiles/study_race_condition.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/study_race_condition.dir/study_race_condition.cpp.o"
+  "study_race_condition"
+  "study_race_condition.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/study_race_condition.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
